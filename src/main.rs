dd4b3497db290e8f3973b406a6427b7b112ae1fc
Austin Riba <austin@m51.io>
2024-09-19 17:51:56 -0700

    End of chapter 3

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+/// This function is called on panic
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    println!("Hello from println! {}", 42);
+    loop {}
+}
